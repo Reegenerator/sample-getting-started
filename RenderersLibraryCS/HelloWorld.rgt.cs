9a90aeb974503907f8dd090d392f8007345962a7
Tedy Pranolo <tedy.pranolo@gmail.com>
2014-01-14 22:24:18 -0800

    Initial

---
+++ RenderersLibraryCS/HelloWorld.rgt.cs
@@ -0,0 +1,21 @@
+namespace RenderersLibraryCS
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using Kodeo.Reegenerator;
+
+    public partial class HelloWorld
+    {
+        /// <summary>
+        /// Method that gets called prior to calling <see cref="Render"/>.
+        /// Use this method to initialize the properties to be used by the render process.
+        /// You can access the project item attached to this generator by using the <see cref="ProjectItem"/> property.
+        /// </summary>
+        public override void PreRender()
+        {
+            base.PreRender();
+            // var projectItem = base.ProjectItem;
+        }
+    }
+}
---
+++ RenderersLibraryCS/XmlToEnum.rgt.cs
@@ -0,0 +1,32 @@
+namespace RenderersLibraryCS
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using Kodeo.Reegenerator;
+    using System.Xml.Linq;
+    public partial class XmlToEnum
+    {
+        XDocument _xdoc;
+        string _className;
+        int _firstValue;
+
+        /// <summary>
+        /// Method that gets called prior to calling <see cref="Render"/>.
+        /// Use this method to initialize the properties to be used by the render process.
+        /// You can access the project item attached to this generator by using the <see cref="ProjectItem"/> property.
+        /// </summary>
+        /// 
+        public override void PreRender()
+        {
+            base.PreRender();
+            //read the source xml
+            _xdoc = XDocument.Parse(System.IO.File.ReadAllText(base.ProjectItem.FullPath));
+            //get className
+            _className = _xdoc.Root.Attribute("ClassName").Value;
+            //get FirstValue
+            var firstValueEle = _xdoc.Root.Attributes("FirstValue").FirstOrDefault();
+            _firstValue = Convert.ToInt32(firstValueEle.Value);
+        }
+    }
+}
